14b16da18692f92e11827ff7aeec85fc0a540a30
Stefano Pernat <stefano.pernat@trentinosviluppo.it>
2024-05-05 14:23:31 +0200

    feat: test figlet

--- lua/plugins/figlet.lua
+++ lua/plugins/figlet.lua
@@ -1,11 +1,17 @@
+--   __ _       _      _           _             _
+--  / _(_) __ _| | ___| |_   _ __ | |_   _  __ _(_)_ __
+-- | |_| |/ _` | |/ _ \ __| | '_ \| | | | |/ _` | | '_ \
+-- |  _| | (_| | |  __/ |_  | |_) | | |_| | (_| | | | | |
+-- |_| |_|\__, |_|\___|\__| | .__/|_|\__,_|\__, |_|_| |_|
+--        |___/             |_|            |___/
 return {
-  "pavanbhat1999/figlet.nvim",
-  dependencies = {
-    "numToStr/Comment.nvim",
-  },
-  config = function()
-    -- require("figlet").setup({
-      -- font = "ogre",
-    -- })
-  end,
+	"pavanbhat1999/figlet.nvim",
+	dependencies = {
+		"numToStr/Comment.nvim",
+	},
+	config = function()
+		-- require("figlet").setup({
+		-- font = "ogre",
+		-- })
+	end,
 }
